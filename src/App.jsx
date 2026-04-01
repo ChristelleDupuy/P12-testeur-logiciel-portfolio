4924029d3229d90dac584345592e2035a0787817
Christelle DUPUY <christelledupuy@MacBook-Pro-de-Christelle.local>
2026-04-01 21:40:01 +0200

    cleanup: remove unused Contact component and improve project section naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Portfolio />
+      <Contact />
     </>
   );
 }
